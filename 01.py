99942f7b741dcbb8541f2f6dd681b2051e2e41f7
1758083479 <1758083479@qq.com>
2019-05-06 20:49:16 +0800

    1

--- 01.py
+++ 01.py
@@ -1,1 +1,28 @@
-print1
+'''
+import sys
+
+for line in sys.stdin:
+    print(eval(line.strip()))
+'''
+
+import sys
+for line in sys.stdin:
+    a,b =line.strip().split('-')
+    flag = 0
+    a1 = len(a)
+    b1= len(b)
+    result = ''
+    for i in range(a1):
+        an = int(a[a1-1-i])
+        if b1-i-1<0:
+            bn=0
+        else:
+            bn= int(b[b1-1-i])
+        cn = an-bn-flag
+        flag = 0
+        if cn<0:
+            cn+=10
+            flag=1
+        result = str(cn) + result
+
+    print(result)
